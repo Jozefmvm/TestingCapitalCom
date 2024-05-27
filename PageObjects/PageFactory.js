82916284e57484a03d5220d1f8e248ccc68ef1e6
Jozefmvm <murvitalymvm@gmail.com>
2024-05-27 20:19:00 +0300

    added loop for enviroments

--- PageObjects/PageFactory.js
+++ PageObjects/PageFactory.js
@@ -3,6 +3,7 @@ const { Search } = require('../PageObjects/PageComponents/SearchComponent');
 const { Cryptocurrencies } = require('../PageObjects/CryptocurrenciesPage');
 const { Login } = require('../PageObjects/PageComponents/Login');
 const { TradingPlatformPage } = require('../PageObjects/TradingPlatformPage');
+const { Constants } = require('../PageObjects/PageComponents/Constants');
 
 
 class PageFactory {
@@ -14,6 +15,8 @@ class PageFactory {
         this.Cryptocurrencies = new Cryptocurrencies();
         this.Login = new Login();
         this.TradingPlatformPage = new TradingPlatformPage();
+        this.Constants = new Constants();
+
     }
 }
 
--- test/US/US_01.06.test.js
+++ test/US/US_01.06.test.js
@@ -16,32 +16,51 @@ describe('US_01.06', () => {
 
     })
 
-    let countries = ['Germany','Arab Emirates','Australia'];
-    let licences = ['Cyprus / EU (Regulated by CYSEC)','(Regulated by SCB)', 'Australia (Regulated by ASIC)'];
+    let countries = ['Germany'/*,'Arab Emirates','Australia'*/];
+    
+    let licences = ['CYSEC','SCB', 'ASIC'];
+
+    let languages = ['English', 'Arabic', 'German', 'Greek', 'Spanish', 'French', 'Italian', 'Hungarian', 'Dutch', 'Polish', 'Romanian', 
+'Russian', 'New Zealand', 'Chinese'];
 
 
     for (let country of countries){
+        for (let language of languages){
 
-        describe (`US_01.06 - ${country}`, () => {
+        describe (`US_01.06 - ${country} + ${language}`, () => {
 
 
-            it (`Should be appropriate name of licence in browser line after change country to ${country}`, async () => {
+            it (`Should be appropriate name of licence in browser line after change country to ${country} + ${language}`, async () => {
 
                 let index = countries.indexOf(country)
+                let index_lang = languages.indexOf(language)
                 let variantOfLicence = [
                     {country: pageFactory.header.germanyCountry, language: pageFactory.header.languageEn},
-                    {country: pageFactory.header.arabEmiratesCountry, language: pageFactory.header.languageEn},
-                    {country: pageFactory.header.australiaCountry, language: pageFactory.header.languageEn}
+                    {country: pageFactory.header.germanyCountry, language: pageFactory.header.languageAr},
+                    {country: pageFactory.header.germanyCountry, language: pageFactory.header.languageDe},
+                    {country: pageFactory.header.germanyCountry, language: pageFactory.header.languageEl},
+                    {country: pageFactory.header.germanyCountry, language: pageFactory.header.languageEs},
+                    {country: pageFactory.header.germanyCountry, language: pageFactory.header.languageFr},
+                    {country: pageFactory.header.germanyCountry, language: pageFactory.header.languageIt},
+                    //{country: pageFactory.header.germanyCountry, language: pageFactory.header.languageHu},
+                    //{country: pageFactory.header.germanyCountry, language: pageFactory.header.languageNl},
+                    //{country: pageFactory.header.germanyCountry, language: pageFactory.header.languagePl},
+                    //{country: pageFactory.header.germanyCountry, language: pageFactory.header.languageRo},
+                    //{country: pageFactory.header.germanyCountry, language: pageFactory.header.languageRu},
+                    //{country: pageFactory.header.germanyCountry, language: pageFactory.header.languageZn},
+                    //{country: pageFactory.header.germanyCountry, language: pageFactory.header.languageCn},
+                    //{country: pageFactory.header.arabEmiratesCountry, language: pageFactory.header.languageEn},
+                    //{country: pageFactory.header.australiaCountry, language: pageFactory.header.languageEn}
                 ];
-                await pageFactory.header.SelectCountryAndLanguage(await variantOfLicence[index].country, await variantOfLicence[index].language);
+                await pageFactory.header.SelectCountryAndLanguage(await variantOfLicence[index].country, await variantOfLicence[index_lang].language);
                 await pageFactory.header.CheckLicence();
-                await expect(pageFactory.header.licence).toHaveText(licences[index]);
+                await expect(pageFactory.header.licence).toHaveComputedLabel(expect.stringContaining(licences[0]));
                 
             })
 
 
-            it(`US_01.06!00_01 ${country} Unregistered user should be Sign Up form is opened after_Click button [Start Trading Now] `,async() =>{
-
+            it(`US_01.06!00_01 ${country} + ${language} Unregistered user should be Sign Up form is opened after_Click button [Start Trading Now] `,async() =>{
+                
                 await pageFactory.header.GoToCryptocurrencies();
                 await pageFactory.Cryptocurrencies.startTradingNowButton.click();
                 await expect(pageFactory.Cryptocurrencies.singUpForm).toBeDisplayed();
@@ -51,8 +70,8 @@ describe('US_01.06', () => {
             })
 
 
-            it(`US_01.06!00_01 ${country} Authorized user should be The trading platform page is opened after_Click button [Start Trading Now] `,async() =>{
-
+            it(`US_01.06!00_01 ${country} + ${language}  Authorized user should be The trading platform page is opened after_Click button [Start Trading Now] `,async() =>{
+                
                 await pageFactory.Login.LoggingFunction(process.env.USEREMAIL, process.env.PASSWORD);
                 await pageFactory.TradingPlatformPage.CloseModalIcon();
                 await expect(pageFactory.TradingPlatformPage.themeSwitherTradingPlatform).toBeDisplayed();
@@ -61,8 +80,8 @@ describe('US_01.06', () => {
             })
 
 
-            it(`US_01.06!00_01 ${country} Unauthorized user should be Login form is opened`,async() =>{
-
+            it(`US_01.06!00_01 ${country} + ${language}  Unauthorized user should be Login form is opened`,async() =>{
+                
                 await pageFactory.Login.LoggingFunction(process.env.USEREMAIL, process.env.PASSWORD);
                 await pageFactory.TradingPlatformPage.CloseModalIcon();
                 await pageFactory.Login.LogoutFunctionTrading();
@@ -78,5 +97,6 @@ describe('US_01.06', () => {
         })
 
     }
+}
     
 })
